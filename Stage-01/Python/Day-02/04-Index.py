f078d5bfde716ceb3aec0ccbc2b4bcdf12017202
vrundthumar <vrundthumar@gmail.com>
2025-11-29 10:16:40 +0530

    Update

---
+++ Stage-01/Python/Day-02/04-Index.py
@@ -0,0 +1,16 @@
+# Sets
+# It cannot have duplicate value
+# its order is not fixed
+
+cars = {"BMW", "Audi", "Pagani"}
+
+
+numbers = {1, 2, 3, 2, 4, 4}
+print(numbers)  # {1, 2, 3, 4}
+
+# How tp add element in to sets
+cars.add("Bugati")
+numbers.add(5)
+
+# How to remove element
+cars.remove("Audi")
