6f902863e63161e063497efb6cb3c4dababbd537
vrundthumar <vrundthumar@gmail.com>
2025-11-29 10:08:53 +0530

    Update

---
+++ Stage-01/Python/Day-02/03-Index.py
@@ -0,0 +1,12 @@
+# Tuples
+# They are faster
+# It is same as list but can't change
+
+fruits = ["Apple", "Orange", "Mango"]
+# Index      0         1        2 
+
+# To retrive elements
+print(fruits[0])  # Apple
+
+
+fruits[0] = "Graps" # Error
